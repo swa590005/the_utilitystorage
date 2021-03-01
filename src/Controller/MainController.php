b63497c99dfd0f4e983f5a6035d5b7cfc927fce2
Swati Joshi <swatijoshi@outlook.de>
2021-03-01 22:33:14 +0100

    required annotations, make and created controller and displayed hello world

---
+++ src/Controller/MainController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="main")
+     */
+    public function index(): Response
+    {
+        return new Response('<h1>OMG! My first page already! WOOO!</h1>');
+    }
+}
