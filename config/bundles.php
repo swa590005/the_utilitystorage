9c384385e113607f8c04740d10d78c551f1e283a
Swati Joshi <swatijoshi@outlook.de>
2021-03-01 22:57:34 +0100

    required twig

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
