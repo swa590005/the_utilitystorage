d3476e8f9b9d5ad6484b08b8167ea861b1372350
Swati Joshi <swatijoshi@outlook.de>
2021-03-03 11:39:40 +0100

    created meterform and list to show

--- src/Controller/AdminMeterController.php
+++ src/Controller/AdminMeterController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\MeterFormType;
 
 class AdminMeterController extends AbstractController
 {
@@ -15,4 +16,15 @@ class AdminMeterController extends AbstractController
     {
         return $this->render('admin_meter/index.html.twig', []);
     }
-}
+
+    /**
+     * @Route("/admin/meter/new", name="admin_meter_new")
+     */
+    public function new()
+    {
+        $form = $this->createForm(MeterFormType::class);
+        return $this->render('admin_meter/new.html.twig', [
+            'meterForm' => $form->createView()
+        ]);
+    }
+} 
