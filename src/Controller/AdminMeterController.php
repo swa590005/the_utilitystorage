6251f73bdf85dff985866e7b426fa18917dcdefd
Swati Joshi <swatijoshi@outlook.de>
2021-03-05 11:55:02 +0100

    created meter and list view

--- src/Controller/AdminMeterController.php
+++ src/Controller/AdminMeterController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Meter;
+use App\Repository\MeterRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Form\MeterFormType;
@@ -13,11 +17,41 @@ class AdminMeterController extends AbstractController
     /**
      * @Route("/admin/meter/new", name="admin_meter_new")
      */
-    public function new()
+    public function new(EntityManagerInterface $em, Request $request): Response
     {
+
+
         $form = $this->createForm(MeterFormType::class);
+
+        //handle request comes into picture during post
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $meter=$form->getData();
+
+            $em->persist($meter);
+            $em->flush();
+
+            $this->addFlash('success', 'Meter Created!');
+
+
+           return $this->redirectToRoute('admin_meter_list');
+
+        }
         return $this->render('admin_meter/new.html.twig', [
             'meterForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/meter", name="admin_meter_list")
+     */
+    public function list(MeterRepository $meterRepository): Response
+    {
+        $meters=$meterRepository->findAll();
+        return $this->render('admin_meter/list.html.twig',[
+            'meters'=>$meters,
         ]);
     }
 } 
--- src/Form/MeterFormType.php
+++ src/Form/MeterFormType.php
@@ -2,15 +2,27 @@
 
 namespace App\Form;
 
+use App\Entity\Meter;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MeterFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('meter_number')
+            ->add('meter_number',TextType::class,[
+                'help'=> 'Enter Meter No'
+            ])
         ;
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        //This binds the form to that class.
+        $resolver->setDefaults([
+            'data_class'=> Meter::class
+        ]);
     }
 }
