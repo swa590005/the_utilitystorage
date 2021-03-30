df930e318d8d60cc839d39e5e23a6f0675f6e474
Swati Joshi <swatijoshi@outlook.de>
2021-03-30 18:03:19 +0200

    added csrf protection and remember me while login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -26,4 +26,12 @@ class SecurityController extends AbstractController
             'error' => $error
         ]);
     }
+
+    /**
+     * @Route ("/logout", name="app_logout")
+     * @throws \Exception
+     */
+    public function logout(){
+        throw new \Exception('Will be intercepted before getting here');
+    }
 }
