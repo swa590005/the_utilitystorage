f9d26bb3dc70d0fc676a91d13327ecaeb482a59d
Swati Joshi <swatijoshi@outlook.de>
2021-04-01 17:45:46 +0200

    abstract class to get user

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * @method User getUser()
+ */
+abstract class BaseController extends AbstractController
+{
+
+}
